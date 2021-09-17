7d83e263c0cafdfedba7d76eacc75b2636a7ff13
Giacomo Volini <giacomo.volini@gmail.com>
2021-09-17 11:21:32 +0200

    Classi Player e Costanti

---
+++ src/main/java/jmb/model/Player.java
@@ -0,0 +1,65 @@
+package jmb.model;
+
+import static jmb.ConstantsClass.*;
+
+
+public class Player {
+
+    //VARIABILI DI ISTANZA
+    private String name;
+    private boolean isWhite;
+    private int level;
+    private String imgName;
+
+    //COSTRUTTORE CON NOME IMMAGINE SPECIFICATO
+    public Player( String name, String imgName, boolean isWhite)  {
+
+        this.name = name;
+        //TODO implementare logica presenza file o meno
+        this.imgName = imgName;
+        this.level = 1;
+        this.isWhite = isWhite;
+
+    }
+
+    //COSTRUTTORE CON IMMAGINI DI DEFAULT
+    public Player ( String name, boolean isWhite){
+
+        this.name = name;
+        this.level = 1;
+        this.isWhite = isWhite;
+        if (this.isWhite) {
+            this.imgName = IMG_DEF_WHITE;
+        } else {
+            this.imgName = IMG_DEF_BLACK;
+        }
+
+    }
+
+    //METODI PER ACCEDERE A VARIABILI D'ISTANZA DALL'ESTERNO
+
+    public boolean getIsWhite() {
+        return this.isWhite;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    public String getImgName() {
+        return this.imgName;
+    }
+
+    //METODI PER MODIFICARE LE VARIABILI D'ISTANZA DALL'ESTERNO
+
+    public void setLevel( int level) {
+        if (level<0) {
+            level = level*-1;
+        }
+        this.level=level;
+    }
+}

caf873359ebb24ae3879023acd5b0118ecac9272
Ameen <ameen.daaboul@gmail.com>
2021-09-18 11:48:07 +0200

    classe Tabellone: metodo isBlackExit metodo isWhiteExit

--- src/main/java/jmb/model/Tabllone.java
+++ src/main/java/jmb/model/Tabllone.java
@@ -7,6 +7,10 @@ public class Tabllone {
     private boolean BlackExit;
     private boolean WhiteTurn;
 
+public Tabllone (){
+    this.BlackExit = false;
+    this.WhiteExit = false;
+}
 
     public boolean mossaPossibile(int puntaInizC, int puntaInizR, int puntaFinR, int puntaFinC){
 
@@ -26,6 +30,47 @@ public class Tabllone {
         if(mossaPossibile(puntaInizC, puntaInizR, puntaFinR, puntaFinC)){
             caselle [puntaFinR][puntaFinC]= caselle[puntaInizR][puntaInizC];
             caselle[puntaInizR][puntaInizC]= null;
+        }
+    }
+
+    public void isBlackExit() {
+
+        // i=colnne  j=righe
+
+
+            if (!this.BlackExit){
+                boolean trovato = false;
+
+                for (int i=1; i<=18 && !trovato; i++){
+                    for (int j=0; j<=1 && !trovato; j++){
+                        if (caselle [j][i] != null && !caselle [j][i].getisWhite()){
+                            trovato = true;
+                        }
+                    }
+                }
+                if (!trovato){
+                    BlackExit = true;
+                }
+            }
+    }
+    public void isWhiteExit() {
+
+        // i=colnne  j=righe
+
+
+        if (!this.WhiteExit){
+            boolean trovato = false;
+
+            for (int i=7; i<=24 && !trovato; i++){
+                for (int j=0; j<=1 && !trovato; j++){
+                    if (caselle [j][i] != null && caselle [j][i].getisWhite()){
+                        trovato = true;
+                    }
+                }
+            }
+            if (!trovato){
+                WhiteExit = true;
+            }
         }
     }
 }

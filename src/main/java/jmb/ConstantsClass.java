a4f59f4989a6da71b9591d2a7c98ade52e2322a0
Ameen <ameen.daaboul@gmail.com>
2021-09-22 16:05:12 +0200

    tutto per tutto

--- src/main/java/jmb/ConstantsClass.java
+++ src/main/java/jmb/ConstantsClass.java
@@ -3,6 +3,8 @@ package jmb;
 public final class ConstantsClass {
 
     public static final int LVL_ONE = 1;
+    public static final int COL_WHITE = 1;
+    public static final int COL_BLACK = 24;
     public static final String IMG_DEF_WHITE = "img-def-white";
     public static final String IMG_DEF_BLACK = "img-def-black";
 

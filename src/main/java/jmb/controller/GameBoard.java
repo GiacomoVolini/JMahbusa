043c178d90d67ffa278aa1ab79db8613efd018e2
Giacomo Volini <giacomo.volini@gmail.com>
2021-10-06 12:09:07 +0200

    Versione Prova Gameboard

--- src/main/java/jmb/controller/GameBoard.java
+++ src/main/java/jmb/controller/GameBoard.java
@@ -189,35 +189,42 @@ public class GameBoard {
     protected Region[] regArrayTop;
     protected Region[] regArrayBot;
 
-    public GameBoard() {
-        super();
-        this.polArrayTop = new Polygon[] {this.point_1, this.point_2, this.point_3, this.point_4, this.point_5, this.point_6,
-                this.point_7, this.point_8, this.point_9, this.point_10, this.point_11, this.point_12};
+    //public GameBoard() {
+      //  super();
+        //this.polArrayTop = new Polygon[] {this.point_1, this.point_2, this.point_3, this.point_4, this.point_5, this.point_6,
+          //      this.point_7, this.point_8, this.point_9, this.point_10, this.point_11, this.point_12};
         /*this.polArrayBot = new Polygon[] {point_24, point_23, point_22, point_21, point_20, point_19,
-                point_18, point_17, point_16, point_15, point_14, point_13};
-        this.regArrayTop = new Region[] {point_1_R, point_2_R, point_3_R, point_4_R, point_5_R, point_6_R,
-                point_7_R, point_8_R, point_9_R, point_10_R, point_11_R, point_12_R};
-        this.regArrayBot = new Region[] {point_24_R, point_23_R, point_22_R, point_21_R, point_20_R, point_19_R,
+                point_18, point_17, point_16, point_15, point_14, point_13};*/
+        //this.regArrayTop = new Region[] {this.point_1_R, this.point_2_R, this.point_3_R, this.point_4_R, this.point_5_R, this.point_6_R,
+          //      this.point_7_R, this.point_8_R, this.point_9_R, this.point_10_R, this.point_11_R, this.point_12_R};
+        /*this.regArrayBot = new Region[] {point_24_R, point_23_R, point_22_R, point_21_R, point_20_R, point_19_R,
                 point_18_R, point_17_R, point_16_R, point_15_R, point_14_R, point_13_R};*/
-    }
+    //}
 
     public void initialize() {
 
+        this.polArrayTop = new Polygon[] {this.point_1, this.point_2, this.point_3, this.point_4, this.point_5, this.point_6,
+                      this.point_7, this.point_8, this.point_9, this.point_10, this.point_11, this.point_12};
+        this.regArrayTop = new Region[] {this.point_1_R, this.point_2_R, this.point_3_R, this.point_4_R, this.point_5_R, this.point_6_R,
+                     this.point_7_R, this.point_8_R, this.point_9_R, this.point_10_R, this.point_11_R, this.point_12_R};
         window.widthProperty().addListener((obs, oldVal, newVal) -> {
             boardRect.setLayoutX((window.getWidth())/3);
             boardRect.setWidth((window.getWidth())/3);
             for (int i=0; i<12; i++) {
-                //regArrayTop[i].setLayoutX(boardRect.getLayoutX()+(i*(boardRect.getWidth())));
-                //regArrayTop[i].setPrefWidth((boardRect.getWidth())/24);
-                /*polArrayTop[i].setLayoutX(boardRect.getLayoutX()+(i*(boardRect.getWidth())));
+                /*DEBUG System.out.println (polArrayTop[i]);
+                System.out.println (regArrayTop[i]);*/
+                regArrayTop[i].setLayoutX(boardRect.getLayoutX()+(i*(boardRect.getWidth())));
+                regArrayTop[i].setPrefWidth((boardRect.getWidth())/12);
+
+                polArrayTop[i].setLayoutX(boardRect.getLayoutX()+(i*(boardRect.getWidth()/12)));
                 polArrayTop[i].getPoints().setAll(0d, 0d,
-                        (i+1)*(boardRect.getWidth()), 0d,
-                        (i*(boardRect.getWidth())+boardRect.getWidth()/2), boardRect.getY()+regArrayTop[i].getPrefHeight() );*/
-                point_1.setLayoutX(boardRect.getLayoutX());
+                        (boardRect.getWidth()/12), 0d,
+                        (i*(boardRect.getWidth()/12)+(boardRect.getWidth()/24)), boardRect.getY()+regArrayTop[i].getPrefHeight() );
+                /*point_1.setLayoutX(boardRect.getLayoutX());
                 point_1.getPoints().setAll(0d, 0d,
                         boardRect.getWidth(), 0d,
                         boardRect.getWidth()/48, boardRect.getY()+point_1_R.getPrefHeight() );
-
+                */
             }
 
         });

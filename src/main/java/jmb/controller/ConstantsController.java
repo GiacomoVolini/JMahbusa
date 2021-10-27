69891e5a197b8e37a6b35c78b1d26c7cd89ae7a2
Ameen <ameen.daaboul@gmail.com>
2021-10-27 17:19:48 +0200

    Ho messo i numeri in parole

--- src/main/java/jmb/controller/ConstantsController.java
+++ src/main/java/jmb/controller/ConstantsController.java
@@ -4,7 +4,10 @@ public class ConstantsController {
 
     public static final double HORIZONTAL_RESIZE_FACTOR = 0.6;      //La larghezza massima che il tabellone può occupare rispetto alla finestra
     public static final double VERTICAL_RESIZE_FACTOR = 0.9;        //L'altezza massima che il tabellone può occupare rispetto alla finestra
-
+    public static final double FIN_BTN_WIDTH = 111;                 //La larghezza massima del buttone "finish turn"
+    public static final double SMAL_BTN_WIDTH = 71;                 //La larghezza massima dei buttoni piccoli
+    public static final double FIN_BTN_HIGHT = 80;                  //L'altezza massima del buttone "finish turn"
+    public static final double SMAL_BTN_HIGHT = 38;                 //L'altezza massima dei buttoni piccoli
     private ConstantsController() {
         throw new AssertionError() ;
     }

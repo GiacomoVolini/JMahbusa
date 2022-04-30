095e124db5c325ef29b5726668251e0936c6e0a3
GiacomoV <giacomo.volini@gmail.com>
2022-04-30 13:14:59 +0200

    30/04/2022
    Completato ennesimo refactor di BoardViewRedraw per evitare metodo initialize e sua chiamata in App

--- src/main/java/jmb/view/View.java
+++ src/main/java/jmb/view/View.java
@@ -51,7 +51,7 @@ public class View implements IView {
 
     @Override
     public void setPawnsForTurn() {
-        BoardViewRedraw.redrawPawns();
+        BoardViewRedraw.redrawPawns(sceneBoard);
     }
 
     @Override

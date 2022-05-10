6470be8877f103a3c39c94c3b40eb9ea7ef95944
Ameen <ameen.daaboul@gmail.com>
2022-05-10 11:29:16 +0200

    corretto indirizzo

--- src/main/java/jmb/view/Musica.java
+++ src/main/java/jmb/view/Musica.java
@@ -8,13 +8,13 @@ import java.io.File;
 
 public class Musica {
 
-    String musicaPartita = new File("C:\\Users\\Ameen\\IdeaProjects\\JMahbusa\\src\\main\\resources\\jmb\\view\\partita.mp3").toURI().toString();
+    String musicaPartita = new File("/jmb/view/partita.mp3").toURI().toString();
     MediaPlayer playerp = new MediaPlayer( new Media(musicaPartita));
 
-    String musicaMenu = new File("C:\\Users\\Ameen\\IdeaProjects\\JMahbusa\\src\\main\\resources\\jmb\\view\\musicaMenu.mp3").toURI().toString();
+    String musicaMenu = new File("/jmb/view/musicaMenu.mp3").toURI().toString();
     MediaPlayer player = new MediaPlayer( new Media(musicaMenu));
 
-    String musicaPawn = new File("C:\\Users\\Ameen\\IdeaProjects\\JMahbusa\\src\\main\\resources\\jmb\\view\\Pawns.mp3").toURI().toString();
+    String musicaPawn = new File("/jmb/view/Pawns.mp3").toURI().toString();
     AudioClip Pawn = new AudioClip(musicaPawn);
 
   //  String musicaPawn = new File("C:\\Users\\Ameen\\IdeaProjects\\JMahbusa\\src\\main\\resources\\jmb\\view\\Pawns.mp3").toURI().toString();

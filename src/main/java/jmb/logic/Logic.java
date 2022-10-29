644c30bfddae7099b86ae2d4c916eb7d4fba11fb
Ameen <ameen.daaboul@gmail.com>
2022-10-29 12:19:28 +0200

    29/10/2022

--- src/main/java/jmb/logic/Logic.java
+++ src/main/java/jmb/logic/Logic.java
@@ -22,7 +22,6 @@ public class Logic implements ILogic {
 
     private void createApplicationFolders() {
         try {
-
             Files.createDirectories(Path.of(getAppDirectory() + "/leaderboard"));
             Files.createDirectories(Path.of(getAppDirectory() + "/saves"));
             Files.createDirectories(Path.of(getAppDirectory() + "/settings"));
@@ -32,7 +31,6 @@ public class Logic implements ILogic {
             ioe.printStackTrace();
         }
     }
-
     private void placeLanguageFiles() {
         System.out.println("PRIMA DEL RTY");
         try {
@@ -61,12 +59,10 @@ public class Logic implements ILogic {
             ioe.printStackTrace();
         }
     }
-
     @Override
     public void initializeGameLogic() {
         board = new GameLogic();
     }
-
     @Override
     public void initializeLeaderboardLogic() {
         try {
@@ -74,9 +70,7 @@ public class Logic implements ILogic {
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-
     }
-
     @Override
     public void initializeTutorialLogic() {
         board = new TutorialLogic();
@@ -90,7 +84,6 @@ public class Logic implements ILogic {
         settings = new SettingsLogic();
         initializeStringsReader();
     }
-
     @Override
     public void initializeStringsReader() {
         strings = new StringsReader();
@@ -99,72 +92,57 @@ public class Logic implements ILogic {
     public String getAppDirectory() {
         return appDirectory;
     }
-
     @Override
     public boolean getWhichTurn() {
         return board.isWhiteTurn();
     }
-
     @Override
     public void placePawnOnPoint(int whichPoint) {
         board.movePawn(board.getMoveBufferColumn(), whichPoint);
     }
-
     @Override
     public boolean isLastOnPoint(int whichPoint, int whichRow) {
         boolean last = whichRow == 15 || this.getBoardPlaceState(whichPoint, whichRow + 1) == EMPTY;
         return last;
     }
-
     @Override
     public void nextTurn() {
         ((GameLogic)board).changeTurn();
     }
-
     @Override
     public int getBoardPlaceState(int whichPoint, int whichRow) {
         return board.getBoardPlaceState(whichPoint, whichRow);
     }
-
     @Override
     public void createMoveBuffer(int whichPoint) {
         board.setMoveBuffer(whichPoint, board.searchTopOccupiedRow(whichPoint));
     }
-
     @Override
     public List<Player> getPlayerList() {
         return ldb.getList();
     }
-
     @Override
     public boolean isRollDouble() {
         return board.getDice().getDoubleNum();
     }
-
     @Override
     public int[] getDiceValues() {
         return board.getDice().getDiceValues();
     }
-
     @Override
     public void firstTurn() {
         board.runTurn();
     }
-
     @Override
     public List<String> getPlayerNameList() {
         return ldb.getNameList();
     }
-
     @Override
     public int compareNameLists(String newName1, String newName2) {
         return ldb.compareNameLists(newName1, newName2);
     }
-
     @Override
     public void addNewPlayersToList(String newName1, String newName2) {
-        //  Se i due nomi non contengono il carattere di escape "\u2001" in coda essi sono nuovi.
-        //  Si crea quindi un nuovo oggetto Player contenente quel nome e lo si aggiunge alla PlayerList
         if (!newName1.contains("\u2001")) {
             ldb.addNewPlayer(newName1);
         }
@@ -172,43 +150,34 @@ public class Logic implements ILogic {
             ldb.addNewPlayer(newName2);
         }
     }
-
     @Override
     public boolean isDiceUsed (int i) {
-
         return board.getDice().getUsed(i);
     }
-
     @Override
     public void setUpNewBoard() {
         board.setUp();
     }
-
     @Override
     public void revertMove() {
         ((GameLogic)board).revertMove();
     }
-
     @Override
     public void setWhiteExit(boolean value) {
         board.setWhiteExit(value);
     }
-
     @Override
     public void setBlackExit(boolean value) {
         board.setBlackExit(value);
     }
-
     @Override
     public boolean getWhiteExit() {
         return board.getWhiteExit();
     }
-
     @Override
     public boolean getBlackExit() {
         return board.getBlackExit();
     }
-
     @Override
     public void setPlayersForGame(String whitePlayer, String blackPlayer) {
         GameLogic game = (GameLogic)board;
@@ -217,156 +186,126 @@ public class Logic implements ILogic {
         game.setBlacksWonPoints(0);
         game.setWhitesWonPoints(0);
     }
-
     @Override
     public void setPlayersForGame(String whitePlayer, String blackPlayer, int tournamentPoints) {
         setPlayersForGame(whitePlayer, blackPlayer);
         ((GameLogic)board).setTournamentPoints(tournamentPoints);
     }
-
     @Override
     public String getWhitePlayer() {
         return ((GameLogic)board).getWhitePlayer();
     }
-
     @Override
     public String getBlackPlayer() {
         return ((GameLogic)board).getBlackPlayer();
     }
-
     @Override
     public void addStatsToLeaderboard() {
         GameLogic game = (GameLogic)board;
         ldb.addStatsToList(game.getWhitePlayer(), game.getBlackPlayer(), game.getWhitesWonPoints());
         ldb.addStatsToList(game.getBlackPlayer(), game.getWhitePlayer(), game.getBlacksWonPoints());
     }
-
     @Override
     public boolean isTournamentOngoing() {
         return (((GameLogic)board).getTournamentPoints() != 0);
     }
-
     @Override
     public int getWhiteTournamentPoints() {
         return ((GameLogic)board).getWhitesWonPoints();
     }
-
     @Override
     public int getBlackTournamentPoints() {
         return ((GameLogic)board).getBlacksWonPoints();
     }
-
     @Override
     public int getTournamentPointsToWin() {
         return ((GameLogic)board).getTournamentPoints();
     }
-
     @Override
     public void setTurnDuration(double value) {
         ((GameLogic)board).setTurnDuration(value);
     }
-
     @Override
     public double getTurnDuration() {
         return ((GameLogic)board).getTurnDuration();
     }
-
     @Override
     public void saveGame(String saveName) {
         SaveGameWriter.writeSaveFile(((GameLogic)board), saveName);
     }
-
     @Override
     public List<String> getSaveList() {
         return SaveGameReader.getSaveList();
     }
-
     @Override
     public SaveGameReader readSaveGame(String saveName) {
         return SaveGameReader.readSaveGame(saveName);
     }
-
     @Override
     public String[] getLoadViewData(String saveName) {
         return this.readSaveGame(saveName).getLoadViewData();
     }
-
     @Override
     public int searchTopOccupiedRow(int col) {
         return board.searchTopOccupiedRow(col);
     }
-
     @Override
     public boolean isPawnMovable(int col, int row, boolean highlight) {
         return board.isPawnMovable(col, row, highlight);
     }
-
     @Override
     public void deleteSaveFile(String fileName) {
         SaveGameWriter.deleteSaveFile(fileName);
     }
-
     @Override
     public boolean isSaveNamePresent(String saveName) {
         return SaveGameReader.isSaveNamePresent(saveName);
     }
-
     @Override
     public void setUpSavedGame(String saveName) {
         ((GameLogic)board).setUpSavedGame(SaveGameReader.readSaveGame(saveName));
     }
-
     @Override
     public int[][] getBoardMatrix() {
         int[][] out = null;
         out = board.getSquares();
         return out;
     }
-
     @Override
     public void setCanRevert (boolean value) {
         ((GameLogic)board).setCanRevert(value);
-    }
-    @Override
+    }    @Override
     public void selectPawn(int whichPoint, int whichRow) {
         board.selectPawn(whichPoint, whichRow);
     }
-
     @Override
     public void deselectPawn(int col, int row) {
         board.deselectPawn(col, row);
     }
-
     @Override
     public boolean movePawn(int from, int to) {
         return board.movePawn(from, to);
     }
-
     @Override
     public int[][] getSaveMatrix(String saveName) {
         return this.readSaveGame(saveName).getSquareMatrix();
     }
-
     @Override
     public boolean getGameStart() {
         return board.getGameStart();
     }
-
     @Override
     public boolean getGameEndState() {
         return ((GameLogic)board).getGameEndState();
     }
-
     @Override
     public void setGameStart(boolean value) {
         ((GameLogic)board).setGameStart(value);
     }
-
     @Override
     public void setGameEndState(boolean value) {
         ((GameLogic)board).setGameEndState(value);
     }
-
     @Override
     public boolean allDiceUsed() {
         boolean used = true;
@@ -375,7 +314,6 @@ public class Logic implements ILogic {
                 used = false;
         return used;
     }
-
     @Override
     public void completeMoves() {
         ((GameLogic)board).completeMoves();
@@ -389,18 +327,15 @@ public class Logic implements ILogic {
             return false;
         }
     }
-
     @Override
     public void resetDefaultSettings() {
         settings.getDefaultSettings();
         settings.restoreCurrent();
     }
-
     @Override
     public void applySettingsChanges() {
         settings.applySettingsChanges();
     }
-
     @Override
     public void nextTutorialStage() {
         ((TutorialLogic)board).nextTutorialStage();
@@ -473,5 +408,4 @@ public class Logic implements ILogic {
     public void setcurrentlanguage(int num){
         StringsReader.setcurrentlanguage(num);
     }
-
 }
